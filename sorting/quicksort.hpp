d58027e9d707b8d91a2aea1a05e7656b39e228d5
Mikkkee <boy.swordman@gmail.com>
2016-05-20 19:09:33 +0800

    Format with clang-format

--- sorting/quicksort.hpp
+++ sorting/quicksort.hpp
@@ -1,12 +1,10 @@
-template <typename T>
-void swap(T &a, T &b) {
+template <typename T> void swap(T &a, T &b) {
   T tmp = a;
   a = b;
   b = tmp;
 }
 
-template <typename T>
-int partition(T &a, int lo, int hi) {
+template <typename T> int partition(T &a, int lo, int hi) {
   auto pivot = a[hi];
   int i = lo;
   int j = lo;
@@ -23,11 +21,10 @@ int partition(T &a, int lo, int hi) {
   return i;
 }
 
-template <typename T>
-void quicksort(T &a, int lo, int hi) {
+template <typename T> void quicksort(T &a, int lo, int hi) {
   if (lo < hi) {
     int p = partition(a, lo, hi);
-    quicksort(a, lo, p-1);
-    quicksort(a, p+1, hi);
+    quicksort(a, lo, p - 1);
+    quicksort(a, p + 1, hi);
   }
 }
